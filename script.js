122894883e7264d76eb4b3f56c6cae1bc26b1c92
CrownSheep <106444061+CrownSheep@users.noreply.github.com>
2026-08-07 03:48:44 +0300

    Add line count display

--- script.js
+++ script.js
@@ -4,24 +4,26 @@ const tierInfo = document.getElementById("tier-info");
 
 tierInfo.style.display = "none";
 
-fileInput.onchange = () => {
+fileInput.onchange = async () => {
     tierInfo.style.display = "inline-block";
 
     let total = 0, list = "";
     for (const file of fileInput.files) {
+        const text = await file.text();
+        const lines = text.split("\n").length;
+        list += `${file.name} | ${lines} lines | ${(file.size / 1024).toFixed(2)} KB<br>`;
         total += file.size;
-        list += `${file.name} | ${(file.size / 1024).toFixed(2)} KB<br>`;
     }
 
     const size = total / 1024;
-    fileInfo.innerHTML = `${list}<br><b>Total: ${size.toFixed(2)} KB (${total} Bytes)</b>`;
+    fileInfo.innerHTML = `${list}<br><b>Total: ${size.toFixed(2)} KB (${total} Bytes)<br></b>`;
 
     const tier = size <= 2 ? "Ultra Hard" : size <= 5 ? "Hard" : size <= 15 ? "Standard" : "";
 
     if (!tier) {
         tierInfo.innerHTML = `<span class="fail">This project is over 15KB ):</span>`;
     } else {
-        const cssClass = tier.toLowerCase().replace(" ", "-");
-        tierInfo.innerHTML = `This project is <span class="${cssClass}">${tier}</span> tier.`;
+        const tierClass = tier.toLowerCase().replace(" ", "-");
+        tierInfo.innerHTML = `This project is <span class="${tierClass}">${tier}</span> tier.`;
     }
 };
